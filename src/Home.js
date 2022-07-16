70e8ea853941fef42403ea73b1a2901d27dc817c
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-07-17 00:06:43 +0530

    feat: removing handleDelete

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,6 @@ import BlogList from './BlogList';
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
-    
-
-    const handleDelete = (id) =>{
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
 
     useEffect( () =>{
         fetch('http://localhost:8000/blogs')
@@ -25,7 +19,7 @@ const Home = () => {
 
     return (
         <div className="home">
-            {blogs && < BlogList blogs = { blogs } title = "All Blogs!" handleDelete={handleDelete} />}
+            {blogs && < BlogList blogs = { blogs } title = "All Blogs!" />}
         </div>
     );
 }
