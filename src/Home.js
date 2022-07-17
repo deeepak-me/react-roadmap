10135b178339fadf7a789eed83502a562808f1de
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-07-17 16:51:54 +0530

    feat : error handling message

--- src/Home.js
+++ src/Home.js
@@ -6,16 +6,26 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect( () =>{
         setTimeout(()=>{
             fetch('http://localhost:8000/blogs')
             .then(res => {
+
+                if(!res.ok){
+                   throw Error('could not fetch data'); 
+                };
                 return res.json();
             })
             .then(data => {
                 setBlogs(data);
                 setIsPending(false);
+                setError(null);
+            })
+            .catch((err) => {
+                setIsPending(false);
+                setError(err.message);
             });
         },1000)
     },[]);
@@ -23,6 +33,7 @@ const Home = () => {
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
             {blogs && < BlogList blogs = { blogs } title = "All Blogs!" />}
         </div>
