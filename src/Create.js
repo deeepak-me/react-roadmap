eba67b11059af8b6028b5071ca8606c8c7d14da0
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-07-19 15:49:35 +0530

    feat: programmatic redirection completed

--- src/Create.js
+++ src/Create.js
@@ -1,16 +1,31 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
 
     const[title, setTitle] = useState('');
     const[body, setBody] = useState('');
     const[author, setAuthor] = useState('mario');
+    const [isPending, setIsPending] = useState(false);
+    const history = useHistory();
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
         const blog = {title, body, author};
 
-        console.log(blog);
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs',{
+
+            method:'POST',
+            headers:{"content-type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() =>{
+            console.log('new blog added');
+            setIsPending(false);
+            history.push('/');
+        })
     }
 
 
@@ -39,8 +54,9 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
-
+                { !isPending && <button>Add Blog</button> }
+                { isPending && <button disabled>Adding Blog...</button> }
+            
             </form>
         </div>
     );
