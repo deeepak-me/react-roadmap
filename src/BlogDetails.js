15d0627e2efd00eac54f6d2fcb69058dad2a67f9
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-07-20 00:08:26 +0530

    feat: blog delete function completed

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,24 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
+import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
 
     const { id } = useParams()
     const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+    const history = useHistory();
+
+    const handleClick= () =>{
+        fetch('http://localhost:8000/blogs/' + blog.id,{
+
+            method: 'DELETE'
+
+        }).then(() =>{
+
+            history.push('/');
+
+        })
+    }
 
     return (
         <div className="blog-details">
@@ -15,6 +29,7 @@ const BlogDetails = () => {
                     <h2>{ blog.title }</h2>
                     <p> Written By { blog.author }</p>
                     <div>{ blog.body }</div>
+                    <button onClick={handleClick}>Delete</button>
                 </article>
                 
             )}
